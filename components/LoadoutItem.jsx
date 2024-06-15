bd3fc37280ecceade8624025309e8ca9d043fd8d
Aroon Salotra <aroonsalotra@gmail.com>
2024-06-15 14:22:49 +0100

    add LoadoutItem

---
+++ components/LoadoutItem.jsx
@@ -0,0 +1,12 @@
+const LoadoutItem = () => {
+  return (
+    <button>
+      <figure>
+        <i></i>
+        <figcaption>Weapon</figcaption>
+      </figure>
+    </button>
+  );
+};
+
+export default LoadoutItem;
